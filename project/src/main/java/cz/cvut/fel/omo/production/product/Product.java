60d72d8459bc4e8163ac968072c0578c11f3b7bd
Pavel Pakhomov <pakhopav@fel.cvut.cz>
2020-01-06 09:50:41 +0100

    Product class changes caused by new operations system

--- project/src/main/java/cz/cvut/fel/omo/production/product/Product.java
+++ project/src/main/java/cz/cvut/fel/omo/production/product/Product.java
@@ -1,29 +1,60 @@
 package cz.cvut.fel.omo.production.product;
 
-import cz.cvut.fel.omo.production.Production;
 import cz.cvut.fel.omo.transactions.Money;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Product implements Reportable {
     public final ProductType type;
-    public final Production myProduction;
+    //    public final Production myProduction;
     public final Money myPrice;
+    private int id;
     private final Reportable[] components;
+    private List<Operation> myOperations = new ArrayList<>();
 
-    public Product(ProductType type, Product[] components, Production production) {
+    public Product(ProductType type, Product[] components, int id) {
         this.type = type;
         this.components = components;
-        myProduction = production;
+        this.id = id;
+//        myProduction = production;
         int price = 0;
+
         for (Product p : components) {
             price += p.myPrice.amount;
         }
         myPrice = new Money(price + 1);
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public List<Operation> getMyOperations() {
+        return myOperations;
+    }
+
     @Override
     public void report() {
-        System.out.println();
+        for (Operation operation : myOperations) {
+            System.out.println(operation.toString());
+        }
+        if (components.length != 0) {
+            System.out.println("Was made from products:");
+        }
+        int i = 0;
+        for (Reportable reportable : components) {
+            reportable.report();
+            if (i != components.length - 1) System.out.println(",");
+            i++;
+
+        }
     }
+
+    public void addOperation(Operation operation) {
+        myOperations.add(operation);
+    }
+
 
 
 }
